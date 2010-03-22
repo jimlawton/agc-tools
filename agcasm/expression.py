2c18e40fa500f89e65edf0dd9319c68bb01191d7
Jim Lawton <jim.lawton@gmail.com>
2010-03-22 21:25:34 +0000

    Class for AGC expressions.

---
+++ agcasm/expression.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+# Copyright 2010 Jim Lawton <jim dot lawton at gmail dot com>
+# 
+# This file is part of pyagc. 
+#
+# This is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this software; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+from number import *
+
+class Expression:
+    """Class that represents an AGC expression."""
+    
+    def __init__(self, expr):
+        self.valid = False
+        self.complete = False
+        self.expr = expr
+
+    def isValid(self):
+        return self.valid
+
+    def isComplete(self):
+        return self.complete
