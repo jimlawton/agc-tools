085ec0ea3435f4f36d454b2717de585bee856882
jim.lawton <devnull@localhost>
2011-04-27 16:54:33 +0100

    Remove obsolete import.

--- agcasm/opcodes.py
+++ agcasm/opcodes.py
@@ -19,7 +19,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 from architecture import Architecture
-from opcode import OpcodeType, OperandType, OperandOption
+from opcode import OpcodeType, OperandType
 from memory import AddressType
 from instruction import Instruction
 from directive import Directive
